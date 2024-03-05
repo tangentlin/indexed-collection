61175a5f1953a562d3c7a4616564fd065cbff23f
Tangent Lin <unknown>
2024-03-05 18:57:48 -0500

    Export additional types
    Summary:
    
    Test Plan:

---
+++ src/core/index.ts
@@ -0,0 +1,13 @@
+export { CollectionNature } from './CollectionNature';
+export { ICollectionOption } from './ICollectionOption';
+export { ICollectionViewOption } from './ICollectionViewOption';
+export { IMutableCollection } from './IMutableCollection';
+export { IReadonlyCollection } from './IReadonlyCollection';
+export { KeyExtract, MultipleKeyExtract, SingleKeyExtract } from './KeyExtract';
+export { Nullable, Optional } from './Optional';
+export { defaultCollectionOption } from './defaultCollectionOption';
+export {
+  defaultFilter as defaultCollectionViewFilter,
+  defaultCollectionViewOption,
+  defaultSort as defaultCollectionViewSort,
+} from './defaultCollectionViewOption';
