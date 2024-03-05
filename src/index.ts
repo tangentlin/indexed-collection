9681b897f5afb3b482a0667649d8ebaea4255f17
Tangent Lin <>
2024-03-05 18:57:48 -0500

    Export additional types
    Summary:
    
    Test Plan:

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
 export { CollectionViewBase } from './collections/CollectionViewBase';
 export { IndexedCollectionBase } from './collections/IndexedCollectionBase';
 export { PrimaryKeyCollection } from './collections/PrimaryKeyCollection';
-export { ICollectionOption } from './core/ICollectionOption';
-export { ICollectionViewOption } from './core/ICollectionViewOption';
-export {
-  KeyExtract,
-  MultipleKeyExtract,
-  SingleKeyExtract,
-} from './core/KeyExtract';
-export { Nullable, Optional } from './core/Optional';
-export { defaultCollectionOption } from './core/defaultCollectionOption';
-export {
-  defaultFilter as defaultCollectionViewFilter,
-  defaultCollectionViewOption,
-  defaultSort as defaultCollectionViewSort,
-} from './core/defaultCollectionViewOption';
+export * from './core';
 export { CollectionIndex } from './indexes/CollectionIndex';
 export { IndexBase } from './indexes/IndexBase';
 export * from './signals';
