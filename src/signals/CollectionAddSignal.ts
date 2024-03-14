f160d4007acfb3a78fb4114e061a11868f68c0c5
Tangent Lin <>
2024-03-14 05:31:13 -0400

    feat: Add granular change signals
    Summary:
    * Add details to Change signal, providing details such as what have been added, removed or updated
    * Add separate signals of add/remove/update
    
    Test Plan:
    * Verify unit tests have passed
    * Create collection/collection view, and verify add/remove/update/change signals work as expected.

---
+++ src/signals/CollectionAddSignal.ts
@@ -0,0 +1,12 @@
+import { IReadonlyCollection } from '../core/IReadonlyCollection';
+import { Signal } from './Signal';
+
+export class CollectionAddSignal<T> extends Signal {
+  static readonly type = Symbol('COLLECTION_ADD');
+  constructor(
+    target: IReadonlyCollection<T>,
+    public readonly added: readonly T[]
+  ) {
+    super(CollectionAddSignal.type, target);
+  }
+}
